1149ad67f38b9b442a5bc813b84c0395d18d582f
Art <artemgoryunov89@gmail.com>
2023-10-08 17:34:57 +0400

    Написали программу для формирования массива строк

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число элементов");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+
+string[] FillArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"Введите строку №{i+1}:");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+string[] PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    return arr;
+}
+
+void CreateNewArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length<=3)
+        {
+            Console.Write($"{arr[i]} ");
+        }
+    }
+}
+string[] result1 = FillArray(array);
+Console.WriteLine ("Исходный массив: ");
+string[] result2 = PrintArray(result1);
+Console.WriteLine();
+Console.WriteLine ("Новый массив: ");
+CreateNewArray(result2);
+
+
+
